58fd32750508439150700544175bbed24495a726
MarkusMagpie <matveysorokin@ngs.ru>
2022-12-22 17:07:11 +0700

    Intro for the game

--- My_Turret_Game/Intro_game.py
+++ My_Turret_Game/Intro_game.py
@@ -1,5 +1,20 @@
-import pygame
-
-
+# переменная screen - экран
 def intro():
-    font = pygame.font.Font()
+    font = pygame.font.Font(None, 35)
+    text = font.render('Об игре', 1, 'white')
+    text_x = 210
+    text_y = 300
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, 'white', (text_x - 10, text_y - 10, text_w + 20,
+                                       text_h + 20), 5)
+
+    text = font.render('Играть', 1, 'white')
+    text_x = 225
+    text_y = 370
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, 'white', (text_x - 10, text_y - 10, text_w + 20,
+                                       text_h + 20), 5)
